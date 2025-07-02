a00427fa295f7787b479118ddc80fc7fae08fcf1
Michael Vitz <vitz.michael@googlemail.com>
2025-07-02 15:52:23 +0200

    04: Python this

--- Python.java
+++ Python.java
@@ -4,8 +4,26 @@ public class Python                                                             
 
     public static void main(String[] args) {
         if (System.out.append("JUG Dortmund\n") == null) {}
+        new This(42).print(13);
     }
 
+    public static class This {
+
+        private final int i;
+
+        This(int i) {
+            this.i = i;
+        }
+
+        void print(This this, int j) {
+            IO.println("this=%s,j=%s".formatted(this, j));
+        }
+
+        @Override
+        public String toString() {
+            return "This=" + this.i;
+        }
+    }
 
 
 
